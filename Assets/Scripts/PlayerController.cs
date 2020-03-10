78a1013f7129eb59d2288facfd188b4fc1c8ddb1
Naoki Kita <naokikita12@yahoo.com>
2020-03-09 19:04:36 -0700

    adding boarders

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,8 +18,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Shoot();
         Move();
+        BoundMovement();
+
+
+        Shoot();
     }
 
     void Move()
@@ -37,7 +40,25 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            GameObject.Instantiate(this.bullet, this.transform);
+            GameObject.Instantiate(bullet, transform.position, transform.rotation);
         }
+    }
+
+    void BoundMovement()
+    {
+        float dist = (this.transform.position - Camera.main.transform.position).z;
+
+        float leftBoarder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, dist)).x;
+        float rightBoarder = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, dist)).x;
+        float topBoarder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, dist)).y;
+        float bottomBoarder = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, dist)).y;
+
+        Vector3 playerSize = GetComponent<Renderer>().bounds.size;
+
+        this.transform.position = new Vector3(
+        Mathf.Clamp(this.transform.position.x, leftBoarder + playerSize.x / 2, rightBoarder - playerSize.x / 2),
+        Mathf.Clamp(this.transform.position.y, topBoarder + playerSize.y / 2, bottomBoarder - playerSize.y / 2),
+        this.transform.position.z
+        );
     }
 }
